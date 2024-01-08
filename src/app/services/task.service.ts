a0d48f1ecadeee435788ac1f03b43648bf02f940
Jeff <you@example.com>
2024-01-08 22:59:02 +0300

    Added get task to services

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -1,18 +1,22 @@
 import { Injectable } from '@angular/core';
-import { Observable, of } from 'rxjs';
+import { Observable } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 import { Task } from '../Task';
-import { TASKS } from '../mock-task';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class TaskService {
+  private apiUrl:string = 'http://localhost:5000/tasks';
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
 
   getTask(): Observable<Task[]> {
-    return of(TASKS);
+    return this.http.get<Task[]>(this.apiUrl);
   }
+
+  
 
 }
